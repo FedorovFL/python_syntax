c48d0f3d92da399c83ce8e72572fe066dbfb6f8d
FedorovFL <fedorfl1980@mail.ru>
2024-04-04 15:03:02 +0300

    added 2_var

---
+++ 2_var.py
@@ -0,0 +1,47 @@
+# однострочный коммент
+# "ctrl+/"
+# print("hello world")
+# print("hell")
+# print("thailand")
+
+'''какой то 
+многострочный 
+коммент'''
+
+"""i am
+a 
+millionnair"""
+
+# переменные
+
+# var_1 = 100
+# print(var_1)
+# snake_case
+# number_of_apples = 10
+# camelCase
+# numberOfBananas = 20
+
+# константы
+# CONST_VAR = 1000
+
+# вызов встроенной функции вывода в терминал
+# print(numberOfBananas, CONST_VAR)
+
+# встроенная функция ввода из терминала
+# my_input = input("Введите что-то: ")
+
+# print("Вы ввели вот это -". my_input)
+
+# небольшой калькулятор
+
+num_1 = input("введите 1 число: ")
+num_2 = input("введите 2 число: ")
+
+# конкатенация строк
+# int
+result = num_1 + num_2 
+
+# конвертация данных
+result = int(num_1) + int(num_2)
+
+print("Результат =", result)
